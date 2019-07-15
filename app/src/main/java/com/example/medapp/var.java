10e495d93aa8a8e7bd2a418e3f7819281f3d1c76
Наталия <nataliapautova@gmail.com>
2019-07-15 20:28:34 +0300

    15,07,19

--- app/src/main/java/com/example/medapp/var.java
+++ app/src/main/java/com/example/medapp/var.java
@@ -1,10 +1,15 @@
 package com.example.medapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,6 +20,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeoutException;
 
 import static com.example.medapp.ActivitiesController.ConverBase64;
 import static com.example.medapp.ActivitiesController.FillActivity;
@@ -28,6 +35,7 @@ public class var extends AppCompatActivity {
     private Button next;
     private  Button foto;
     private ImageView image;
+    private static final int CAMERA_REQUEST = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,16 +93,42 @@ public class var extends AppCompatActivity {
         if(viewGroup.getId()==R.id.checkbox) {
             //check
             LinearLayout ll = (LinearLayout) viewGroup;
-            CheckBox[] mas = new CheckBox[ll.getChildCount()];
-            int count = 0;
+            final CheckBox[] mas = new CheckBox[ll.getChildCount()];
+            final int[] count = {0};
 
             for (int i = 0; i < mas.length; i++) {
                 mas[i] = (CheckBox) ll.getChildAt(i);
                 if (mas[i].isChecked())
-                    count++;
+                    count[0]++;
             }
 
-            String[] options = new String[count];
+            if(count[0] ==0)
+            {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(var.this);
+                builder.setTitle("Внимание!")
+                        .setMessage("Вы не выбрали ни одного варианта ответа!")
+                        .setCancelable(false).
+                        setNegativeButton("Ок",
+                                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            count[0] = 1;
+                            mas[0].setChecked(true);
+                    }
+                });
+                builder.setPositiveButton("Отмена", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                            onPause();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+            String[] options = new String[count[0]];
 
             for (int i = 0; i < mas.length; i++) {
                 int j = 0;
@@ -108,10 +142,37 @@ public class var extends AppCompatActivity {
         }
           else  if(viewGroup.getId()==R.id.radio) {
                 //radio
+            final int[] Id = {-1};
                 RadioGroup rg = (RadioGroup) viewGroup;
                 String[] option = new String[1];
-                int Id = rg.getCheckedRadioButtonId();
-                RadioButton rb = findViewById(Id);
+                Id[0] = rg.getCheckedRadioButtonId();
+                if(Id[0] ==-1)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(var.this);
+                    builder.setTitle("Внимание!")
+                            .setMessage("Вы не выбрали ни одного варианта ответа!")
+                            .setCancelable(false).
+                            setNegativeButton("Ок",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                            Id[0] =1;
+                                            RadioButton rb = findViewById(Id[0]);
+                                            rb.setChecked(true);
+                                        }
+                                    });
+                    builder.setPositiveButton("Отмена", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            onPause();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
+                }
+                RadioButton rb = findViewById(Id[0]);
                 option[0] = rb.getTag().toString();
 
                 NextQuestion(this, option, base);
@@ -122,13 +183,56 @@ public class var extends AppCompatActivity {
                     NextQuestion(this, opt,base);
         }
 
-        //Intent intent = getIntent();
-        //    finish();
-        //    startActivity(intent);
     }
 
     private void Foto(){
         //открыть камеру и передать фото в imageView
+        Intent camera = new Intent();
+        camera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, CAMERA_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            // Фотка сделана, извлекаем картинку
+            Bitmap thumbnailBitmap = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(thumbnailBitmap);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        BackToMain();
+    }
+
+    private void BackToMain(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(var.this);
+        builder.setTitle("Вернуться к начальному экрану???")
+                .setMessage("Усли вы продолжите, то весь прогресс будет утерян безвозвратно. Хотите продолжить?");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Продолжить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent main = new Intent(var.this, MainActivity.class);
+                startActivity(main);
+                //метод обнуления индексов
+            }
+        });
+        builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                onPause();
+            }
+        });
+        AlertDialog main = builder.create();
+        main.show();
 
     }
 
